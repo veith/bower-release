db9f2ac0a05c08e17877605bb952fa75cf80701d
veith <veith.zaech@adcubum.com>
2016-08-12 13:16:12 +0200

    feat(index.js): Automatic commit

--- index.js
+++ index.js
@@ -3,30 +3,69 @@ var fs = require('fs');
 const chalk = require('chalk');
 var inquirer = require('inquirer');
 var bowerfile = JSON.parse(fs.readFileSync('bower.json', 'utf8'));
-var git = require("nodegit");
 
-var version = bowerfile.version.split('.');
-var user = process.env.USER;
-var pwd = process.env.PWD;
-var git = require('simple-git')( pwd );
+const version = bowerfile.version.split('.').map((e) => parseInt(e))
+const user = process.env.USER;
+const pwd = process.env.PWD;
+const git = require('simple-git')(pwd);
 
-console.log('\n\nHi ' + process.env.USER + ' Current version of ' + chalk.yellow(bowerfile.name) + ' is ' + chalk.yellow(bowerfile.version) );
-git.tag(function(err, log) {
-        console.log(log);
-      });
-var questions = [{
+console.log('\n\nHi ' + process.env.USER + ' Current version of ' + chalk.yellow.bold(bowerfile.name) + ' is ' + chalk.yellow(bowerfile.version));
+
+const questions = [
+  {
+    type: 'confirm',
+    name: 'tests',
+    message: 'Sind alle tests durchgelaufen?',
+    default: false
+  },
+  {
     type: 'list',
-    name: 'change',
+    name: 'type',
     message: 'Was für einen Release möchtest du publizieren?',
     choices: ['Patch', 'Feature', 'Breacking'],
     filter: function (val) {
       return val.toLowerCase();
     }
   }
-]
+];
 
 
 inquirer.prompt(questions).then(function (answers) {
-  console.log('\nOrder receipt:');
-  console.log(JSON.stringify(answers, null, '  '));
+
+  if (answers.tests) {
+    var newVersion = calculateNewVersionNumber(answers.type, version);
+    bowerfile.version = newVersion;
+
+    fs.writeFile('bower.json', JSON.stringify(bowerfile), function functionName() {
+      git.add(['./bower.json'], function (i) {
+        git.commit('bower-release: Version ' + newVersion + ' released', function (i) {
+          addTag(newVersion)
+        });
+      });
+
+
+    });
+
+    console.log(chalk.yellow(newVersion));
+
+  } else {
+    console.log(chalk.red('Wenn die Tests nicht durchlaufen macht es keinen Sinn eine neue Version zu releasen.'));
+    process.exit(1);
+  }
+
+
 });
+
+
+function addTag(version, handler) {
+  git.addTag('v' + version, (e) => {
+    git.pushTags('origin');
+  }, handler);
+}
+
+function calculateNewVersionNumber(type, currentversion) {
+  var types = ['breacking', 'feature', 'patch']
+  currentversion[types.indexOf(type)]++;
+  return currentversion.fill(0, types.indexOf(type) + 1).join('.');
+
+}
